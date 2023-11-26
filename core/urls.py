7afc1014e519a4c7c2488839ca76973f1cadfa31
themezoz <themezoz.com@gmail.com>
2023-11-25 22:27:40 -0800

    init

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index , name='index'),
-    path('product/<int:id>', views.product_detials , name='product_detials'),
+    path('product', views.product_detials , name='product'),
     path('store', views.store , name='store'),
     path('checkout', views.checkout , name='checkout'),
 ]
